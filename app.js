683491196603fa205991fcb46475caf2e6d40b9b
khammett325995 <khammett325995@gmail.com>
2025-06-25 18:37:15 -0700

    Created a functional base navigation bar and all photo folders.

---
+++ app.js
@@ -0,0 +1,8 @@
+function menuPopup() {
+    var menuChange = document.getElementById("myNavigation");
+    if (menuChange === "Navigation") {
+        menuChange += " responsive";
+    } else {
+        menuChange = "Navigation";
+    }
+}
