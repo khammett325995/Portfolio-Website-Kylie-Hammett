21bc812bc7a1f7ab3ac83ee263fb1ddcb95685cc
khammett325995 <khammett325995@gmail.com>
2025-07-03 10:47:55 -0700

    Completed Coding Projects Portfolio page.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 function menuPopup() {
     var menuChange = document.getElementById("myNavigation");
-    if (menuChange === "Navigation") {
-        menuChange += " responsive";
+    if (menuChange.className === "Navigation") { // Check the className string
+        menuChange.className += " responsive"; // Add ' responsive' class
     } else {
-        menuChange = "Navigation";
+        menuChange.className = "Navigation"; // Remove ' responsive' class
     }
+}
+
+function images(imgs) {
+    var expandingImage = document.getElementById("selectedImage");
+    var imageText = document.getElementById("imageText");
+    var imageContainer = document.querySelector(".image-container"); 
+
+    expandingImage.src = imgs.src;
+    imageText.innerHTML = imgs.alt;
+
+    imageContainer.style.display = "flex"; 
 }
