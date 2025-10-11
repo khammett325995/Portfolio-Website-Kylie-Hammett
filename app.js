db46068170e763f52a6e284ec3e6e533c7be78b4
Kylie Hammett <khammett325995@gmail.com>
2025-10-11 13:14:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function menuPopup() {
     var menuChange = document.getElementById("myNavigation");
-    if (menuChange.className === "Navigation") { // Check the className string
-        menuChange.className += " responsive"; // Add ' responsive' class
+    if (menuChange.className === "Navigation") { 
+        menuChange.className += " responsive"; 
     } else {
-        menuChange.className = "Navigation"; // Remove ' responsive' class
+        menuChange.className = "Navigation"; 
     }
 }
 
@@ -16,4 +16,26 @@ function images(imgs) {
     imageText.innerHTML = imgs.alt;
 
     imageContainer.style.display = "flex"; 
+}
+
+function imageChange(selectedImageId, imageTextId, clickedImg) {
+    var expandImg = document.getElementById(selectedImageId);
+    var imgText = document.getElementById(imageTextId);
+
+    expandImg.src = clickedImg.src;
+    imgText.innerHTML = clickedImg.alt;
+
+    document.getElementById(selectedImageId).parentElement.style.display = "block";
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+        const backgroundAudio = document.getElementById('websiteMusic');
+        if (backgroundAudio) { 
+            backgroundAudio.volume = 0.25; 
+        }
+    });
+
+function changeModes() {
+  var element = document.body;
+  element.classList.toggle("dark_mode");
 }
